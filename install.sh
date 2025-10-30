15503a0b16e4d1755ecc3f0d44188ca76a8ebac0
lmaotrix <coco.gian07@gmail.com>
2025-10-30 17:10:00 +0100

    add install script using stow

---
+++ install.sh
@@ -0,0 +1,12 @@
+# define dotfiles directory
+DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# install dependencies
+echo "Installing dependencies..."
+sudo pacman -S --needed zsh neovim starship tmux stow
+
+# use stow to symlink dotfiles
+cd "$DOTFILES_DIR" || exit
+stow bash zsh nvim tmux starship
+
+echo "Dotfiles installed! source your shell config to apply changes."
